833122f027b51a07383b3a732292d5602a682303
ZRJ <1061083419@qq.com>
2021-05-08 23:14:50 +0800

    blog HTML页面成功显示

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-
+const router = require('./router')
 const app = express()
 
 app.use('/public/', express.static(path.join(__dirname, './public/')))
@@ -9,10 +9,6 @@ app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')
 app.engine('html', require('express-art-template'))
 app.set('views', path.join(__dirname, './views/')) // 默认找到的就是 views目录下的文件  可以修改为指定的目录
 
-app.get('/', (req, res) => {
-    res.render('index.html', {
-        msg: '欢迎来到首页!!!'
-    })
-})
+app.use(router)
 
 app.listen(8080, () => console.log('Server is running on http://127.0.0.1:8080'))
