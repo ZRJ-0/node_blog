a7c1e84e6727740aa3e7839025447ad80e910e12
ZRJ <1061083419@qq.com>
2021-05-09 14:37:43 +0800

    blog 同步表单提交(仿Ajax)

--- router.js
+++ router.js
@@ -4,7 +4,13 @@ const router = express.Router()
 const User = require('./models/user')
 
 router.get('/', (req, res) => {
-    res.render('index.html')
+    if (req.session.user) {
+        res.render('index.html', {
+            user: req.session.user
+        })
+    } else {
+        res.render('index.html')
+    }
 })
 
 router.get('/login', (req, res) => {
@@ -16,7 +22,9 @@ router.post('/login', (req, res) => {
 })
 
 router.get('/register', (req, res) => {
-    res.render('register.html')
+    res.render('register.html', {
+        title: '注册页面'
+    })
 })
 
 router.post('/register', (req, res) => {
@@ -36,18 +44,29 @@ router.post('/register', (req, res) => {
         }
         if (data) {
             // 邮箱或者昵称已存在
-            return res.status(200).json({
-                err_code: 1,
-                message: 'Email or Nickname is already exists.'
+            // return res.status(200).json({
+            //     err_code: 1,
+            //     message: 'Email or Nickname is already exists.'
+            // })
+            return res.render('register.html', {
+                // 生成错误信息到注册表格上侧
+                err_msg: '邮箱或密码已存在',
+                // 接受返回post同步表单提交的请求体数据 body 
+                // 然后将其渲染到input表单当中 使得刷新页面还能显示原有的信息 原来没有ajax都是通过这种形式
+                form: body
             })
         }
 
         body.password = md5(md5(body.password))
         new User(body).save((err, user) => {
             if (err) {
-                return res.status(500).json({
-                    err_code: 500,
-                    message: 'Internal Error.'
+                // return res.status(500).json({
+                //     err_code: 500,
+                //     message: 'Internal Error.'
+                // })
+                return res.render('register.html', {
+                    err_msg: 'Internal Error.',
+                    form: body
                 })
             }
             // console.log(user) // 输出的就是body对象中的数据
