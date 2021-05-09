85bf0bd268c143c5d55c9970718374f0f8de2b03
ZRJ <1061083419@qq.com>
2021-05-09 11:14:15 +0800

    blog Register_Finished(md5&session&json)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const router = require('./router')
+const bodyParser = require('body-parser')
+const session = require('express-session')
 const app = express()
 
 app.use('/public/', express.static(path.join(__dirname, './public/')))
@@ -9,6 +11,22 @@ app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')
 app.engine('html', require('express-art-template'))
 app.set('views', path.join(__dirname, './views/')) // 默认找到的就是 views目录下的文件  可以修改为指定的目录
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(session({
+    // 配置加密字符串，它会在原有加密基础之上和这个字符串拼起来去加密
+    // 目的是为了增加安全性，防止客户端恶意伪造
+    secret: 'test',
+    resave: false,
+    saveUninitialized: false // 无论你是否使用 Session ，我都默认直接给你分配一把钥匙
+}))
+
+// 把路由挂在到 app 中
 app.use(router)
 
 app.listen(8080, () => console.log('Server is running on http://127.0.0.1:8080'))
--- router.js
+++ router.js
@@ -1,9 +1,77 @@
 const express = require('express')
+const md5 = require('blueimp-md5')
 const router = express.Router()
+const User = require('./models/user')
 
 router.get('/', (req, res) => {
     res.render('index.html')
 })
 
+router.get('/login', (req, res) => {
+    res.render('login.html')
+})
+
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+    res.render('register.html')
+})
+
+router.post('/register', (req, res) => {
+    const body = req.body
+    User.findOne({
+        $or: [{
+            email: body.email
+        }, {
+            nickname: body.nickname
+        }]
+    }, (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                success: false,
+                message: 'Server Error!!!'
+            })
+        }
+        if (data) {
+            // 邮箱或者昵称已存在
+            return res.status(200).json({
+                err_code: 1,
+                message: 'Email or Nickname is already exists.'
+            })
+        }
+
+        body.password = md5(md5(body.password))
+        new User(body).save((err, user) => {
+            if (err) {
+                return res.status(500).json({
+                    err_code: 500,
+                    message: 'Internal Error.'
+                })
+            }
+            // console.log(user) // 输出的就是body对象中的数据
+
+            // 注册成功 使用Session记录用户的登录状态
+            // 需要在app中现进行配置    才能使用
+            req.session.user = user
+
+            // Express 提供了一个响应方法：json
+            // 该方法接受了一个对象作为参数， 他会自动帮你将对象转化为字符串再发送给浏览器
+            res.status(200).json({
+                err_code: 0,
+                message: 'OK'
+            })
+
+            // 服务端重定向只针对同步请求才有效，异步请求无效
+            // res.redirect('/')
+        })
+
+    })
+})
+
+router.get('/logout', (req, res) => {
+
+})
 
 module.exports = router
